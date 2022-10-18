a8a529064de002fd026b9891480574c3ace65c8f
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-10-18 20:25:03 +0200

    Remove --no-config (which doesn't work on old version of fisher), and the now un-used --debug="*" --debug-stack-frames=5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# See https://github.com/orgs/community/discussions/35527 for background about the problem that this illustrates
+# See https://github.com/orgs/community/discussions/35527 
+# and https://github.com/jorgebucaran/fisher/issues/742
+# for background about the problem that this illustrates.
 
 # GitHub Codespace Creation STDIN runs this hook script with STDIN set to e.g. pipe:[131656]
 ls -l /proc/$$/fd/0
+
+# This would block this script, both if it is run interactively and on GitHub Codespace Creation
+# cat
 
 # One possible way to work around https://github.com/jorgebucaran/fisher/issues/742,
 # is to redirect STDIN to /dev/null like this here; But it's more fine grained to do it
 # only locally for fisher with < /dev/null as done below, instead for everything.
 # exec </dev/null
 
-# This would block this script, both if it is run interactively and on GitHub Codespace Creation
-# cat
-
 # This works just fine (fish --command runs that command, it does NOT start a new interactive shell)
 fish --command="echo hi"
 
 ## https://github.com/jorgebucaran/fisher
 curl -sL https://git.io/fisher -o /tmp/fisher
-# fish --debug="*" --debug-stack-frames=5 --private --no-config --print-rusage-self --command="source /tmp/fisher && fisher install jorgebucaran/fisher"
-fish --private --no-config --command="set fish_trace 1 && source /tmp/fisher && fisher install jorgebucaran/fisher </dev/null"
+fish --private --command="set fish_trace 1 && source /tmp/fisher && fisher install jorgebucaran/fisher </dev/null"
