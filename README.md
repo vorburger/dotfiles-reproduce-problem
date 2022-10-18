d45af7dc6e92aa0213187af40cdc2295d079f5df
Michael Vorburger <mike@vorburger.ch>
2022-10-18 21:22:09 +0300

    Clean Up

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Reproducer for [this issue](https://github.com/orgs/community/discussions/35527) with [personalized GitHub Workspace dotfiles](https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-github-codespaces-for-your-account#dotfiles).
 
-[`bootstrap-EXPECTED-OUTPUT-WHEN-IT-WORKS.txt`](bootstrap-EXPECTED-OUTPUT-WHEN-IT-WORKS.txt) was produced by `./bootstrap.sh >bootstrap-EXPECTED-OUTPUT-WHEN-IT-WORKS.txt 2>&1`.
+TL;DR if you remove the `< /dev/null` from `fisher` in the [`bootstrap.sh`](bootstrap.sh)
+and set (a fork of) this repo as your Dotfiles Repo in GitHub Settings then it will "block"
+the GitHub Codespace workspace setup.
+
+See https://github.com/jorgebucaran/fisher/issues/742
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,19 @@ set -euxo pipefail
 
 # See https://github.com/orgs/community/discussions/35527 for background about the problem that this illustrates
 
-# GitHub Codespace Creation STDIN runs this hook script with STDIN set to a e.g. pipe:[131656]
+# GitHub Codespace Creation STDIN runs this hook script with STDIN set to e.g. pipe:[131656]
 ls -l /proc/$$/fd/0
+
+# One possible way to work around https://github.com/jorgebucaran/fisher/issues/742,
+# is to redirect STDIN to /dev/null like this here; But it's more fine grained to do it
+# only locally for fisher with < /dev/null as done below, instead for everything.
+# exec </dev/null
 
 # This would block this script, both if it is run interactively and on GitHub Codespace Creation
 # cat
 
 # This works just fine (fish --command runs that command, it does NOT start a new interactive shell)
 fish --command="echo hi"
-
-# Do not redirect STDIN to /dev/null to work around bug (?) in Fisher here, because this breaks things later; instead just fisher </dev/null (below)
-##  exec </dev/null
 
 ## https://github.com/jorgebucaran/fisher
 curl -sL https://git.io/fisher -o /tmp/fisher
