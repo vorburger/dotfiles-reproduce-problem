04c6cc3e3edb08445986b17ef2d4f55a7af557fd
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-10-18 17:40:17 +0200

    Try if Fisher with --no-config works any better

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-## https://github.com/jorgebucaran/fisher
-#curl -sL https://git.io/fisher -o /tmp/fisher
-#fish --debug="*" --debug-stack-frames=5 --private --print-rusage-self --command="source /tmp/fisher && fisher install jorgebucaran/fisher"
+# This works just fine (fish --command runs that command, it does NOT start a new interactive shell)
+fish --command="echo hi"
 
-fish --debug="*" --debug-stack-frames=5 --private --print-rusage-self --command="echo hi"
+## https://github.com/jorgebucaran/fisher
+curl -sL https://git.io/fisher -o /tmp/fisher
+fish --debug="*" --debug-stack-frames=5 --private --no-config --print-rusage-self --command="source /tmp/fisher && fisher install jorgebucaran/fisher"
