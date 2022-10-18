cb735688193a03bb3d042735b6d852de932b3108
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-10-18 17:27:00 +0200

    echo hi instead of fisher

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# https://github.com/jorgebucaran/fisher
-curl -sL https://git.io/fisher -o /tmp/fisher
-fish --debug="*" --debug-stack-frames=5 --private --print-rusage-self --command="source /tmp/fisher && fisher install jorgebucaran/fisher"
+## https://github.com/jorgebucaran/fisher
+#curl -sL https://git.io/fisher -o /tmp/fisher
+#fish --debug="*" --debug-stack-frames=5 --private --print-rusage-self --command="source /tmp/fisher && fisher install jorgebucaran/fisher"
+
+fish --debug="*" --debug-stack-frames=5 --private --print-rusage-self --command="echo hi"
